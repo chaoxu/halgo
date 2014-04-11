273f9de6350057a52b6d1afd03f2168954595318
Chao Xu <mgcclx@gmail.com>
2014-04-10 22:56:43 -0500

    abstract out the matching part

--- KMP.hs
+++ KMP.hs
@@ -1,18 +1,12 @@
 --import Data.List
 --import Test.QuickCheck
 --import Data.Algorithms.KMP
-
-isInfixOf' :: Eq a => [a] -> [a] -> Bool
-isInfixOf' pattern text = not $ null $ matchIndices pattern text
-
-matchTails :: Eq a => [a] -> [a] -> [[a]]
-matchTails pattern text = map (`drop` text) (matchIndices pattern text)
-
 data Automaton a = Node {value   :: a,
                          success :: Automaton a,
                          failure :: Automaton a,
                          accept  :: Bool
-                         } | Null {success :: Automaton a}
+                         } | 
+                   Null {success :: Automaton a}
 
 isNull (Null _) = True
 isNull _ = False
@@ -24,30 +18,45 @@ buildAutomaton xs = automaton
           where t = Node x (build xs automaton) (Null automaton) (null xs)
                 build ys s -- s is: success (previous failure)
                   | null ys      = s
-                  | x == value s = s            `nextNode` (failure s)
-                  | otherwise    = (failure' s) `nextNode` s
+                  | x == value s = s          `nextNode` failure s
+                  | otherwise    = failure' s `nextNode` s
                   where (x,xs)       = (head ys, tail ys)
                         nextNode a b = Node x (build xs (success a)) b (null xs)
                         failure' s
                           | isNull s     = s 
                           | x /= value s = failure' (failure s)
                           | otherwise    = s
-                        
+
+matchFold :: Eq a => Automaton a -> [a] -> ([a]->b->b) -> ([a]->b->b) -> b -> b
+matchFold _ [] _ _ identity = identity
+matchFold state text nomat mat identity = match' state text
+  where match' _ [] = identity
+        match' a (x:xs)
+          | not (isNull a) && value a /= x = stay
+          | isNull a                       = nomat (x:xs) next
+          | value a == x && accept a       = mat   (x:xs) next
+          | value a == x                   = nomat (x:xs) next
+          where next = match' (success a) xs
+                stay = match' (failure a) (x:xs)
+
+isInfixOf' :: Eq a => [a] -> [a] -> Bool
+isInfixOf' pattern text
+ | null pattern = True
+ | otherwise    = or $ matchFold (buildAutomaton pattern) text (const (False:)) (const (True:)) []
+
+matchTails :: Eq a => [a] -> [a] -> [[a]]
+matchTails pattern text
+ | null pattern = tails text
+ | otherwise    = result
+  where result = map (pattern++) $ matchFold (buildAutomaton pattern) text (const id) (:) []
 
 matchIndices :: Eq a => [a] -> [a] -> [Int]
-matchIndices pattern text
-  | null pattern = [0..length text]
-  | otherwise    = match' automaton text 1
-  where automaton = buildAutomaton pattern
-        match' _ [] _ = []
-        match' a (x:xs) n
-         | not (isNull a) && value a /= x = match' f (x:xs) n
-         | isNull a                       = match' s xs (n+1)
-         | value a == x && accept a       = [n-length pattern]++match' s xs (n+1)
-         | value a == x                   = match' s xs (n+1)
-         where s = success a
-               f = failure a
-
+matchIndices pattern text 
+ | null pattern = [0..length text]
+ | otherwise    = map (\x -> x-n) result
+  where n = length pattern
+        result = map snd $ filter fst 
+                 $ zip (matchFold (buildAutomaton pattern) text (const (False:)) (const (True:)) []) [1..]
 
 {-
 prop_isSubstringOf :: [Bool] -> [Bool] -> Bool
