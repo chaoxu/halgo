f0cce6262aaf5359e5841a9ff4e7b062240de53d
Chao Xu <mgcclx@gmail.com>
2014-05-08 17:26:42 -0500

    simplified transition function by using fromMaybe.

--- AhoCorasick.hs
+++ AhoCorasick.hs
@@ -1,8 +1,10 @@
-import Data.List
-import Data.Maybe
-import Data.Monoid
-import Data.Function
-import Control.Arrow
+module AhoCorasick ( Automaton(Node), buildAutomaton, match, isInfixOf) where
+import           Control.Arrow (first)
+import           Data.Function (on)
+import           Data.List     (lookup, partition)
+import           Data.Maybe    (fromMaybe, Maybe (..))
+import           Data.Monoid   (All (..), Monoid, getAll, mappend, mconcat,
+                                mempty)
 data Automaton a b = Node {delta  :: a -> Automaton a b,
                            output :: b
                          }
@@ -10,7 +12,7 @@ data Automaton a b = Node {delta  :: a -> Automaton a b,
 equivalentClasses :: (a->a->Bool)->[a]->[[a]]
 equivalentClasses eq = foldl parts []
   where parts [] a = [[a]]
-        parts (x:xs) a 
+        parts (x:xs) a
          | eq (head x) a = (a:x):xs
          | otherwise     = x:parts xs a
 
@@ -19,17 +21,13 @@ buildAutomaton xs = automaton
   where automaton = build (const automaton) xs mempty
 
 build :: (Monoid b,Eq a)=> (a -> Automaton a b) -> [([a],b)] -> b -> Automaton a b
-build trans' xs out = node
-  where node  = Node trans out
-        trans a
-          | isNothing next = trans' a
-          | otherwise      = fromJust next
-          where next = lookup a table
+build trans xs out = node
+  where node  = Node (\x->fromMaybe (trans x) (lookup x table)) out
         table =  map transPair $ equivalentClasses (on (==) (head . fst)) xs
-        transPair xs = (a, build (delta (trans' a)) ys out)
+        transPair xs = (a, build (delta (trans a)) ys out)
          where a  = head $ fst $ head xs
                (ys,zs) = partition (not . null . fst) $ map (first tail) xs
-               out = mappend (mconcat $ map snd zs) (output $ trans' a)
+               out = mappend (mconcat $ map snd zs) (output $ trans a)
 
 match :: Eq a => Automaton a b -> [a] -> [b]
 match a xs = map output $ scanl delta a xs
@@ -37,5 +35,5 @@ match a xs = map output $ scanl delta a xs
 match' :: Eq a => [[a]] -> [a] -> [[[a]]]
 match' pat = match (buildAutomaton $ map (\x-> (x,[x])) pat)
 
-isInfixOf' :: Eq a => [a] -> [a] -> Bool
-isInfixOf' xs ys = getAll $ mconcat $ match (buildAutomaton [(xs, All True)]) ys
+isInfixOf :: Eq a => [a] -> [a] -> Bool
+isInfixOf xs ys = getAll $ mconcat $ match (buildAutomaton [(xs, All True)]) ys
